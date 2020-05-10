750c327a32e522e80c5049f01586ddbefc8f216a
Eric Dalmau <dalmaueric29@gmail.com>
2020-05-10 11:47:39 -0700

    made a change

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const io = socketIo(server);
 io.on('connection', socket => {
   console.log('new websocket')
 
-  socket.emit('message', 'changing')
+  socket.emit('message', 'welcome')
 
   socket.on('sendMessage', message => {
     io.emit('message', message)
